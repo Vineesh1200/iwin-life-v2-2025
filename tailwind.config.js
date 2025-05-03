ec9819af409d26a36f4c57a7c00786335a556758
Vineesh M <vineesh1200@gmail.com>
2025-05-03 17:38:21 +0530

    latest

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ export const theme = {
             grayLightCustom: 'rgba(149, 148, 148, 1)',
         },
         fontFamily: {
-            viga: ['Viga']
+            viga: ['Arial, Helvetica, sans-serif']
         },
         boxShadow: {
             cardBoxShadow: 'rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px'
